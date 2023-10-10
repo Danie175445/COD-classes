9a04b4c8f47348800f93f845142db9f81ff234c2
Daniel Castillo <danny4004@gmail.com>
2023-10-09 23:22:20 -0700

    cleaned up code

--- server/server.js
+++ server/server.js
@@ -4,11 +4,10 @@ const port = 8000;
 const cors = require('cors')
 
 app.use(cors())
-app.use(express.json());                           /* This is new and allows JSON Objects to be posted */
-app.use(express.urlencoded({ extended: true }));   /* This is new and allows JSON Objects with strings and arrays*/
-require('./config/mongoose.config');    /* This is new */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 require('./routes/person.routes')(app)
 require('./routes/class.routes')(app)
 
 
-app.listen(port, () => console.log(`Listening on port: ${port}`) );
+app.listen(port, () => console.log(`Listening on port: ${port}`));
